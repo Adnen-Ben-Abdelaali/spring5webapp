fa731ba9d1905ec2a7d6884f09784c7219484583
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2022-01-07 17:53:35 +0100

    author and book classes added

--- src/main/java/guru/springframework/spring5webapp/Author.java
+++ src/main/java/guru/springframework/spring5webapp/Author.java
@@ -1,4 +1,54 @@
 package guru.springframework.spring5webapp;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Author {
+
+    private String firstName;
+    private String lastName;
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {}
+
+    public Author(String firstName, String lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    /* setters and getters */
+
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+    }
+
+    public void setBooks(Set<Book> books) {
+
+        this.books = books;
+    }
+
+    public Set<Book> getBooks() {
+
+        return this.books;
+    }
+
 }
--- src/main/java/guru/springframework/spring5webapp/Book.java
+++ src/main/java/guru/springframework/spring5webapp/Book.java
@@ -1,4 +1,62 @@
 package guru.springframework.spring5webapp;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Book {
+
+    /*
+        title
+        isbn
+        publisher
+        set of authors
+     */
+
+    private String title;
+    private String isbn;
+    private String publisher;
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {}
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    /* setters and getters */
+
+    public void setTitle(String title) {
+
+        this.title = title;
+    }
+
+    public String getTitle() {
+
+        return this.title;
+    }
+
+    public void setIsbn(String isbn) {
+
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+
+        return this.isbn;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+
+        this.authors = authors;
+    }
+
+    public Set<Author> getAuthors() {
+
+        return this.authors;
+    }
+
 }
